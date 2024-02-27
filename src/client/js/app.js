9810209c3ed035a3a8391a4a4d9b6d63ac00a52b
hijunglit <hijunglit@gmail.com>
2024-02-27 15:13:33 +0900

    set structure

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,22 +1,35 @@
-let intro = document.querySelector("#intro");
-let logo = intro.querySelector(".logo-header");
-let logoSpan = logo.querySelectorAll(".logo");
+console.log("document viewport", document.documentElement.clientHeight);
+console.log("browser window height", window.outerHeight);
+
+const menuBtn = document.querySelector(".siteMenuBtn");
 
 function introAnimation() {
-  setTimeout(() => {
+  let intro = document.querySelector("#intro");
+  if (intro) {
+    let logo = intro.querySelector(".logo-header");
+    let logoSpan = logo.querySelectorAll(".logo");
     setTimeout(() => {
-      logoSpan.forEach((span, idx) => {
-        setTimeout(() => {
-          span.classList.add("fadeout");
-        }, (idx + 1) * 200);
-      });
-    }, 1500);
-    setTimeout(() => {
-      intro.style.opacity = 0;
       setTimeout(() => {
-        intro.remove();
-      }, 500);
-    }, 2000);
-  });
+        logoSpan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.add("fadeout");
+          }, (idx + 1) * 200);
+        });
+      }, 1500);
+      setTimeout(() => {
+        intro.style.opacity = 0;
+        setTimeout(() => {
+          intro.remove();
+        }, 500);
+      }, 2000);
+    });
+  }
 }
+
+function handleMenu() {
+  const menuHeight = document.querySelector(".siteMenu");
+  const page = document.querySelector(".page");
+}
+
 window.addEventListener("DOMContentLoaded", introAnimation);
+menuBtn.addEventListener("click", handleMenu);
