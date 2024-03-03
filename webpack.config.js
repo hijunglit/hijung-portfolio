021c89f9af8c33740f3ff1f46ec4761d8d97fa84
hijunglit <hijunglit@gmail.com>
2024-03-03 11:44:53 +0900

    build app

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const BASE_JS = "./src/client/js/";
 
 module.exports = {
-  entry: "./src/client/js/main.js",
-  mode: "development",
-  watch: true,
+  entry: BASE_JS + "main.js",
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
