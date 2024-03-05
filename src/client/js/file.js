032747e3c4fcb16211591b19d226c7c55d065fcf
hijunglit <hijunglit@gmail.com>
2024-03-05 23:36:38 +0900

    install packge for img optimize by webpack

--- src/client/js/file.js
+++ src/client/js/file.js
@@ -1,3 +1,3 @@
-import profile from "../../images/hijung-profile.jpg";
-import youtubeHome from "../../images/youtube_home.JPG";
-import youtubeSample from "../../images/youtube-sample.gif";
+import profile from "../images/hijung-profile.jpg";
+import youtubeHome from "../images/youtube_home.JPG";
+import youtubeSample from "../images/youtube-sample.gif";
