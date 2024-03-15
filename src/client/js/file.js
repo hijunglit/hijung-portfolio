35bbcdc6cb5bdaf4cbd564ca49fe597d8ea46920
hijunglit <hijunglit@gmail.com>
2024-03-15 18:25:12 +0900

    img source optimize, clean code, update project

--- src/client/js/file.js
+++ src/client/js/file.js
@@ -1,3 +1,6 @@
 import profile from "../images/hijung-profile.jpg";
 import youtubeHome from "../images/youtube_home.JPG";
+import heroCollectionHome from "../images/hero-collection__home.JPG";
 import youtubeSample from "../images/youtube-sample.gif";
+import portfolioSample from "../images/portfolio__sample.gif";
+import heroCollectionSample from "../images/hero-collection__sample.gif";
--- src/controller/pageController.js
+++ src/controller/pageController.js
@@ -8,7 +8,33 @@ export const handleAbout = (req, res) => {
   return res.render("about", { pageTitle: "hijung portfolio | About" });
 };
 export const handleProject = (req, res) => {
-  return res.render("projects", { pageTitle: "hijung portfolio | Projects" });
+  const project = [
+    {
+      name: "youtube clone",
+      img: "/static/images/youtube-sample.gif",
+      desc: "crud를 구현한 동영상 플랫폼 클론 프로젝트",
+      tools: ["node.js", "mongodb", "aws", "fly.io"],
+      link: "https://youtube-2024.fly.dev",
+    },
+    {
+      name: "portfolio",
+      img: "/static/images/portfolio__sample.gif",
+      desc: "프로젝트들을 보여줄 수 있는 웹 포트폴리오",
+      tools: ["pug", "webpack", "responsive", "fly.io"],
+      link: "https://hijung-portfolio.fly.dev",
+    },
+    {
+      name: "hero collection",
+      img: "/static/images/hero-collection__sample.gif",
+      desc: "usestate, useeffect 훅을 이용한 히어로 캐릭터 컬렉션 프로젝트",
+      tools: ["react.js", "react-router", "github.io"],
+      link: "https://hijunglit.github.io/toy-project",
+    },
+  ];
+  return res.render("projects", {
+    pageTitle: "hijung portfolio | Projects",
+    project,
+  });
 };
 export const handleContact = (req, res) => {
   return res.render("contact", { pageTitle: "hijung portfolio | Contact" });
