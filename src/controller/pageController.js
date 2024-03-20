d986e2c303eac6a21e86bec501c5ffcbfceb06ee
hijunglit <hijunglit@gmail.com>
2024-03-20 17:50:44 +0900

    detail page

--- src/controller/pageController.js
+++ src/controller/pageController.js
@@ -2,9 +2,16 @@ const projects = [
   {
     key: "youtube",
     name: "youtube clone",
-    img: "/static/images/youtube-sample.gif",
+    preview: "/static/images/youtube-sample.gif",
+    thumb: "/static/images/youtube__thumbnail.jpg",
     desc: "crud를 구현한 동영상 플랫폼 클론 프로젝트",
     tools: ["node.js", "mongodb", "aws", "fly.io"],
+    goals: [
+      "CRUD 기능을 구현해 웹 개발의 전체적인 흐름에 대한 이해도 향상을 위해",
+      "데이터베이스 프로그램을 사용해 DB 조작 및 쿼리를 사용하기 위해",
+      "관계형 데이터 베이스에 대해 이해하기 위해",
+      "github의 OAuth 기능을 구현하여 auth 시스템을 이해하기 위해",
+    ],
     features: [
       "github 회원가입",
       "댓글 추가/삭제",
@@ -17,9 +24,13 @@ const projects = [
   {
     key: "portfolio",
     name: "portfolio",
-    img: "/static/images/portfolio__sample.gif",
+    preview: "/static/images/portfolio__sample.gif",
+    thumb: "/static/images/portfolio__thumbnail.jpg",
     desc: "프로젝트들을 보여줄 수 있는 웹 포트폴리오",
     tools: ["pug", "webpack", "responsive", "fly.io"],
+    goals: [
+      "진행한 프로젝트 들을 모으고 구체적인 내용을 설명하는 공간을 만들기 위해 제작",
+    ],
     features: [
       "반응형 웹",
       "Home 페이지",
@@ -33,9 +44,14 @@ const projects = [
   {
     key: "hero",
     name: "hero collection",
-    img: "/static/images/hero-collection__sample.gif",
+    preview: "/static/images/hero-collection__sample.gif",
+    thumb: "/static/images/hero__thumbnail.jpg",
     desc: "usestate, useeffect 훅을 이용한 히어로 캐릭터 컬렉션 프로젝트",
     tools: ["react.js", "react-router", "github.io"],
+    goals: [
+      "react.js hook에 대한 이해를 위해 진행",
+      "react.js의 DOM 제어방식을 경험하기 위해",
+    ],
     features: ["반응형 웹", "SPA 라우팅", "useRef hook"],
     url: "https://hijunglit.github.io/toy-project",
     github: "https://github.com/hijunglit/toy-project",
